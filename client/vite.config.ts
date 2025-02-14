9e98089a0c665d94fe70a1e48d81659a89caf781
Chablis Dossou-Gouin - SecuRoad NVC <chablis.dossou-gouin@securoad.be>
2025-02-14 14:57:09 +0100

    github action workflow configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server:{
+    host: '0.0.0.0'
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
