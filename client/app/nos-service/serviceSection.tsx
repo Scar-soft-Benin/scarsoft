133270eb5a6e53ec54f1a06c4dd97a8e8b02e62f
Chablis Dossou-Gouin - MAHCH Solution <omoobaoshoffa@gmail.com>
2025-07-16 11:53:15 +0100

    retrieve reducers and saga

--- client/app/nos-service/serviceSection.tsx
+++ client/app/nos-service/serviceSection.tsx
@@ -1,5 +1,6 @@
 import AppBaseButton from "~/components/appBaseButton";
 import AppBaseTitle from "~/components/appBaseTitle";
+import { motion } from "motion/react";
 
 interface ServiceSectionProps {
     title: string;
@@ -21,12 +22,19 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
     return (
         <div className="my-6 sm:16">
             <AppBaseTitle title={title} subtitle={""} />
+
             <div
                 className={`flex flex-col sm:flex-row ${
                     reverse ? "sm:flex-row-reverse" : ""
                 } items-center justify-around px-4 sm:px-16`}
             >
-                <div className="w-full my-12 sm:w-1/2 mx-8 text-lg">
+                {/* Animate Text Section */}
+                <motion.div
+                    className="w-full my-12 sm:w-1/2 mx-8 text-lg"
+                    initial={{ opacity: 0, x: reverse ? 50 : -50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5, ease: "easeOut" }}
+                >
                     <p className="text-lg sm:text-xl text-justify">
                         {description}
                     </p>
@@ -36,11 +44,20 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
                         textColor="text-dark"
                         bgColor="bg-secondary"
                         type="first"
-                        href={'/contactez-nous'}
+                        href={"/contactez-nous"}
                         className="w-full sm:w-2/3"
                     />
-                </div>
-                <img src={image} alt={title} className="w-full sm:w-1/3 my-4 rounded-xl shadow-lg" />
+                </motion.div>
+
+                {/* Animate Image Section */}
+                <motion.img
+                    src={image}
+                    alt={title}
+                    className="w-full sm:w-1/3 my-4 rounded-xl shadow-lg"
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5, delay: 0.2, ease: "easeOut" }}
+                />
             </div>
         </div>
     );
